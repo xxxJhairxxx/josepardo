6732a2845f1b1f67d2145c831f2ee1a94d003c03
jhair <jinfanzonquispe30@gmail.com>
2024-11-22 09:59:52 -0500

    add swiper testimoios navigation

--- app/components/organisms/AdmisionTestimonios.tsx
+++ app/components/organisms/AdmisionTestimonios.tsx
@@ -5,8 +5,9 @@ import bg from '@/public/images/bg-testimonios.jpg'
 import Image from 'next/image';
 import { Cardtestimonios } from '@/interfaces/admision';
 import TestimonioCard from '../molecules/Admision/TestimonioCard';
-import { Autoplay, Pagination, SwiperOptions } from 'swiper';
+import { Autoplay, Navigation, SwiperOptions } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useEffect, useState } from 'react';
 
 interface props {
     titulo: string;
@@ -14,44 +15,54 @@ interface props {
     testimonios: Cardtestimonios[]
 }
 
+const swiperOptions: SwiperOptions = {
+    navigation: {
+        prevEl: ".admisionTestimonios__container__cards-prev",
+        nextEl: ".admisionTestimonios__container__cards-next",
+    },
+    speed: 700,
+    slidesPerView: "auto",
+    breakpoints: {
+        300: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            slidesPerGroup: 1,
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 60,
+            slidesPerGroup: 1,
+        },
+        1024: {
+            slidesPerView: 1,
+            spaceBetween: 60,
+            slidesPerGroup: 1,
+        },
+        1200: {
+            slidesPerView: 1,
+            spaceBetween: 60,
+            slidesPerGroup: 1,
+        },
+    },
+    pagination: {
+        // el: ".homeBanner-pagination",
+        clickable: true,
+        type: "bullets",
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    modules: [Autoplay, Navigation],
+};
+
 const AdmisionTestimonios = ({ titulo, subtitulo, testimonios }: props) => {
 
-    const SwiperOptions: SwiperOptions = {
-        speed: 700,
-        slidesPerView: "auto",
-        breakpoints: {
-            300: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-                slidesPerGroup: 1,
-            },
-            768: {
-                slidesPerView: 1,
-                spaceBetween: 60,
-                slidesPerGroup: 1,
-            },
-            1024: {
-                slidesPerView: 1,
-                spaceBetween: 60,
-                slidesPerGroup: 1,
-            },
-            1200: {
-                slidesPerView: 1,
-                spaceBetween: 60,
-                slidesPerGroup: 1,
-            },
-        },
-        pagination: {
-            // el: ".homeBanner-pagination",
-            clickable: true,
-            type: "bullets",
-        },
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
-        },
-        modules: [Autoplay, Pagination],
-    };
+
+    const [itemsLoop, setItemsLoop] = useState<boolean>(false);
+    useEffect(() => {
+        setItemsLoop(true);
+    }, []);
 
 
     return (
@@ -62,8 +73,14 @@ const AdmisionTestimonios = ({ titulo, subtitulo, testimonios }: props) => {
                 <Title title={titulo} subtitle={subtitulo} className='admisionTestimonios__container__text' />
 
                 <div className='admisionTestimonios__container__cards'>
-                    
-                    <Swiper {...SwiperOptions} className="mySwiper">
+                    <div className="admisionTestimonios__container__cards-prev ">
+                        <svg className='rotate-180' width="13" height="23" viewBox="0 0 13 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1.92932 20.7251L11.1025 11.5166L1.92932 2.30813" stroke="white" strokeWidth="3.06949" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+
+                    </div>
+
+                    <Swiper {...swiperOptions} loop={itemsLoop} className="mySwiper">
                         {testimonios.map(({ id, imagen, comentario, nombre, ocupacion }) =>
                             <SwiperSlide key={id} className="SwiperSlide">
                                 <TestimonioCard
@@ -75,7 +92,15 @@ const AdmisionTestimonios = ({ titulo, subtitulo, testimonios }: props) => {
                             </SwiperSlide>)
                         }
                     </Swiper>
+
+                    <div className="admisionTestimonios__container__cards-next">
+                        <svg width="13" height="23" viewBox="0 0 13 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1.92932 20.7251L11.1025 11.5166L1.92932 2.30813" stroke="white" strokeWidth="3.06949" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+
+                    </div>
                 </div>
+
             </Container>
         </section>
     );
